e9075f9afa7216628591a8d70791a68cd6304e27
Shubham Arora <shubham@99array.com>
2018-08-25 18:09:22 +0530

    Register UI-Compleed

--- Halanx/Module/Register/HNRegisterViewController.swift
+++ Halanx/Module/Register/HNRegisterViewController.swift
@@ -13,7 +13,7 @@ class HNRegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "REGISTER"
     }
 
 
